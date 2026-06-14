951741180c4c926528f05b5d6fdc4be8a2591c98
Ramchandrasinh <ramchandrasinhgohil9@gmail.com>
2026-06-14 21:24:32 +0530

    remove unnecessary code

--- app/database.py
+++ app/database.py
@@ -1,16 +1,13 @@
 from sqlmodel import SQLModel, create_engine, Session
 from .config import settings
-from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
 engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
-
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
+
 
 def get_db():
     with Session(engine) as session:
